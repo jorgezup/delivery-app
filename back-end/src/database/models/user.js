dcc94e0d84c4b3ceb8b6f307f53fbae6eb0a1023
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-08-23 16:31:45 -0400

    feat: created model

---
+++ back-end/src/database/models/user.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    "User",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      role: DataTypes.STRING,
+    },
+    {
+      timestamps: false,
+    }
+  );
+  User.associate = (models) => {
+    User.hasMany(models.Sale, { foreignKey: "user_id", as: "orders" });
+    User.hasMany(models.Sale, { foreignKey: "seller_id", as: "sales" });
+  };
+  return User;
+};
