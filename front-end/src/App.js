08947a03b466f86672428674f39ce92115ca1f9a
Rachel Tairum <rachel.tairum@gmail.com>
2022-08-24 16:10:34 -0300

    feat: App já com as rotas

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+// import './App.css';
+// import rockGlass from './images/rockGlass.svg';
 
 function App() {
   return (
-    <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
+    <div>
+      <BrowserRouter>
+        <Switch>
+          {/* <Route /> Login */}
+          <Route />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
