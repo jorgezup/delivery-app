85c9c27efac568db277e9605070f3fd3cfca1c5d
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-08-24 18:23:03 -0400

    feat: login service

---
+++ back-end/src/api/services/login.service.js
@@ -0,0 +1,39 @@
+const md5 = require("md5");
+const { User } = require("../../database/models");
+const { generateJWTToken } = require("../../utils/jwt");
+
+const authentication = async ({ email, password }) => {
+  const user = await User.findOne({
+    attributes: ["id", "name", "email", "password", "role"],
+    where: { email },
+  });
+
+  if (!user) {
+    return {
+      status: 404,
+      message: "Invalid fields or user not found!",
+    };
+  }
+
+  const isValidLogin = () => md5(password) === user.dataValues.password;
+
+  if (!isValidLogin()) {
+    return {
+      status: 404,
+      message: "User not found!",
+    };
+  }
+
+  const payload = {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+  };
+
+  const token = generateJWTToken(JSON.stringify(payload));
+
+  return { token };
+};
+
+module.exports = { authentication };
