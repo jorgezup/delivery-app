2a77dc5206f7ba595b0228253b106c70b5e648b9
Rachel Tairum <rachel.tairum@gmail.com>
2022-08-24 19:47:00 -0300

    fix: adiciona url para ligar com a API

--- front-end/src/API_Calls/userLogin.js
+++ front-end/src/API_Calls/userLogin.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const userLogin = async (objBody) => {
-  const url = '...';
+  const url = 'http://localhost:3001/login';
 
   const result = await axios.post(url, objBody)
     .then((response) => response.data).catch((error) => error);
