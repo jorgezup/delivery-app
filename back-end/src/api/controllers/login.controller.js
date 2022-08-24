e6e2583cf91fc60d777ffbf9ce99d1a75a0189db
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-08-24 18:23:30 -0400

    feat: login controller with login and validate

---
+++ back-end/src/api/controllers/login.controller.js
@@ -0,0 +1,21 @@
+const loginService = require("../services/login.service");
+
+const login = async (req, res) => {
+  const { token, status, message } = await loginService.authentication(
+    req.body
+  );
+
+  if (!token) {
+    return res.status(status).json({ message });
+  }
+
+  return res.status(200).json({ token });
+};
+
+const validate = async (req, res) => {
+  const user = res.locals.user;
+  console.log(user);
+  return res.status(200).json({ role: user.role });
+};
+
+module.exports = { login, validate };
