4202559d5971880d20fb599f949f10360d148449
Rachel Tairum <rachel.tairum@gmail.com>
2022-08-24 17:32:56 -0300

    feat:  adiciona eventos a pagina

--- front-end/src/Pages/Login.js
+++ front-end/src/Pages/Login.js
@@ -1,9 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [validEmail, setValidemail] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [disable, setDisable] = useState(true);
+  const [redirectPerson, setRedirectPerson] = useState(false);
+
+  const textError = (emailTest) => {
+    const re = /\S+@\S+\.\S+/; // referência: https://www.horadecodar.com.br/2020/09/13/como-validar-email-com-javascript/
+    const validation = re.test(emailTest); // referência: https://www.horadecodar.com.br/2020/09/13/como-validar-email-com-javascript/
+    if (validation) {
+      setValidemail(false);
+      setDisable(true);
+    } else {
+      setValidemail(true);
+      setErrorMessage('Email Inválido');
+      setDisable(true);
+    }
+  };
+
+  const passwordError = (pass) => {
+    const passLimit = 6;
+    if (pass.length < passLimit) {
+      setValidemail(true);
+      setErrorMessage('Senha Inválida');
+      setDisable(true);
+    } else {
+      setValidemail(false);
+      setDisable(false);
+    }
+  };
+
+  const manageInputs = ({ target }) => {
+    const { value, id } = target;
+
+    if (id === 'email-input') {
+      setEmail(value);
+      textError(value);
+    } else {
+      setPassword(value);
+      passwordError(value);
+    }
+  };
+
+  const clickRegister = () => {
+    setRedirectPerson(true);
+  };
+
   return (
     <div>
-      <h1> Aprovados </h1>
+      <h1> Aprovados App </h1>
       <main>
         <form>
           <label htmlFor="email-input">
@@ -12,6 +61,8 @@ function Login() {
               id="email-input"
               placeholder="Digite seu e-mail"
               data-testid="common_login__input-email"
+              value={ email }
+              onChange={ manageInputs }
             />
           </label>
           <label htmlFor="password-input">
@@ -20,16 +71,27 @@ function Login() {
               id="password-input"
               placeholder="Digite sua senha"
               data-testid="common_login__input-password"
+              value={ password }
+              onChange={ manageInputs }
             />
           </label>
-          <button type="button" data-testid="common_login__button-login">Entrar</button>
+          <button
+            type="button"
+            data-testid="common_login__button-login"
+            disabled={ disable }
+          >
+            Entrar
+          </button>
           <button
             type="button"
             data-testid="common_login__button-register"
+            onClick={ clickRegister }
           >
             Ainda não é cadastrado?
           </button>
         </form>
+        {validEmail && <p>{errorMessage}</p> }
+        {redirectPerson && <Redirect to="/register" /> }
       </main>
     </div>
   );
