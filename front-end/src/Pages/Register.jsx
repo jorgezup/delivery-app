149fedbd547c1ac3d5d081f7c9cfbb6ad36fc138
Isabela Assunção <isa.btssp2015@gmail.com>
2022-08-25 14:28:08 -0300

    feat: register form validations

--- front-end/src/Pages/Register.jsx
+++ front-end/src/Pages/Register.jsx
@@ -1,10 +1,23 @@
 import React, { useState } from 'react';
+import { useEffect } from 'react';
 
 function Register() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const emailRegex = /\S+@\S+\.\S+/;
   // const [pInfo, setPInfo] = useState('');
+  const [disabled, setDisabled] = useState(true);
+
+  function handleButton() {
+    setDisabled(true); setName(''); setEmail(''); setPassword('');
+  };
+
+  useEffect(() => {
+    if (name.length >= 12 && password.length >= 6 && emailRegex.test(email) ) {
+      setDisabled(false);
+    } else { setDisabled(true) }
+  }, [name, email, password]);
 
   return (
     <div className='register-page'>
@@ -13,15 +26,17 @@ function Register() {
           <input
             id='name'
             type="text"
+            data-testid="common_register__input-name"
             placeholder="Nome"
             className="register-input"
             required
             value={ name }
-            onChange={ (e) => setEmail(e.target.value) }
+            onChange={ (e) => setName(e.target.value) }
           />
           <input
             id='email'
             type="text"
+            data-testid="common_register__input-email"
             placeholder="Email"
             className="register-input"
             required
@@ -31,6 +46,7 @@ function Register() {
           <input
             id='password'
             type="password"
+            data-testid="common_register__input-password"
             placeholder="Senha"
             className="register-input"
             required
@@ -39,12 +55,18 @@ function Register() {
           />
           <button
             type="button"
+            data-testid="common_register__button-register"
             className='register-button'
-            onClick={ console.log(email, password, name) }
+            disabled={ disabled }
+            onClick={ handleButton }
           >
-            Entrar
+            Cadastrar
           </button>
-          {/* {pInfo && <p className='login-pInfo'>{pInfo}</p>} */}
+          {/* {pInfo && (
+            <p
+              className='login-pInfo'
+              data-testid="common_register__element-invalid_register"
+            >{pInfo}</p>}) */}
         </form>
       </main>
     </div>
