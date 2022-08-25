d7eed9bd3c14780d392ec9bb41054e87881bf08d
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-08-25 18:30:12 -0400

    feat: added route delete and added authentication and isAdmin validation

--- back-end/src/api/routes/admin.routes.js
+++ back-end/src/api/routes/admin.routes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const adminController = require('../controllers/admin.controller');
 const authentication = require('../middlewares/authentication');
+const validateUser = require('../middlewares/validateUser');
 const isAdmin = require('../middlewares/isAdmin');
 
 const routes = express.Router();
 
-routes.use(isAdmin);
-
-routes.post('/manage', authentication, adminController.createUser);
-routes.get('/manage', authentication, adminController.getAllUsers);
+routes.post('/manage', authentication, isAdmin, validateUser, adminController.createUser);
+routes.get('/manage', authentication, isAdmin, adminController.getAllUsers);
+routes.delete('/manage/:id', authentication, isAdmin, adminController.deleteUser);
 
 module.exports = routes;
