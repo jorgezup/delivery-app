d28083b9b343fb0b154122b9cce23094a279d5db
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-08-25 17:11:03 -0400

    feat: added route register

--- back-end/src/api/routes/index.js
+++ back-end/src/api/routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const loginRoutes = require('./login.routes');
+const registerRoutes = require('./register.routes');
 const adminRoutes = require('./admin.routes');
 
 const routes = express.Router();
 
 routes.use('/login', loginRoutes);
+routes.use('/register', registerRoutes);
 routes.use('/admin', adminRoutes);
 
 module.exports = routes;
