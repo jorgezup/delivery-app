e821e4599bfe9268c8e72ddd1f6d196a4d2a40dd
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-08-25 16:13:13 -0400

    feat: added route admin

--- back-end/src/api/routes/index.js
+++ back-end/src/api/routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const loginRoutes = require('./login.routes');
+const adminRoutes = require('./admin.routes');
 
 const routes = express.Router();
 
 routes.use('/login', loginRoutes);
+routes.use('/admin', adminRoutes);
 
 module.exports = routes;
