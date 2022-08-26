67b803f066946adb670b1d6b3fe2948528c7b2d4
Leticia Missias <leticia.m.icm@gmail.com>
2022-08-26 13:21:21 -0300

    feat: adiciona rota de produtos

--- back-end/src/api/routes/index.js
+++ back-end/src/api/routes/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const loginRoutes = require('./login.routes');
 const registerRoutes = require('./register.routes');
 const adminRoutes = require('./admin.routes');
+const productsRoutes = require('./products.routes');
 
 const routes = express.Router();
 
 routes.use('/login', loginRoutes);
 routes.use('/register', registerRoutes);
 routes.use('/admin', adminRoutes);
+routes.use('/products', productsRoutes);
 
 module.exports = routes;
