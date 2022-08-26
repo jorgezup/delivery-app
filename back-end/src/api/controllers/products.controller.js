f6db8438bf1a7a49e0558b243f96997450ab6de9
Leticia Missias <leticia.m.icm@gmail.com>
2022-08-26 14:45:40 -0300

    adiciona produtos na seeders

--- back-end/src/api/controllers/products.controller.js
+++ back-end/src/api/controllers/products.controller.js
@@ -1,6 +1,7 @@
 const productsService = require('../services/products.service');
 
 const getAllProducts = async (req, res) => {
+  console.log("entrei no controller")
   const products = await productsService.getAllProducts();
 
   return res.status(200).json(products);
