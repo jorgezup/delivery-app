0a8cfc7815b2637996f0441c91fb9464ddb0309b
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-08-26 14:07:47 -0400

    fix: changed baseurl to 3001

--- front-end/src/API_Calls/register.js
+++ front-end/src/API_Calls/register.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:3000';
+axios.defaults.baseURL = 'http://localhost:3001';
 
 async function postRegister(name, email, password) {
   const user = await axios.post('/register', {
