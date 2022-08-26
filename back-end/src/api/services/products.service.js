f61c1620b441930995d240ed08ecf041e4b4b838
Rachel Tairum <rachel.tairum@gmail.com>
2022-08-26 15:52:59 -0300

    fix: underscoreD nas models

--- back-end/src/api/services/products.service.js
+++ back-end/src/api/services/products.service.js
@@ -2,7 +2,8 @@ const { Product } = require('../../database/models');
 
 const getAllProducts = async () => {
   const products = await Product.findAll();
+  console.log(products);
   return products;
 };
 
-module.exports = getAllProducts;
+module.exports = { getAllProducts };
--- back-end/src/database/models/saleProduct.js
+++ back-end/src/database/models/saleProduct.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
     },
     {
       timestamps: false,
-      underscore: true,
+      underscored: true,
       tableName: "salesProducts",
     }
   );
