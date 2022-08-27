37df3af5e6a0d75a9b6f6b157bc732ed0869474c
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-08-27 18:20:06 -0400

    feat: added header, form create user and table user

--- front-end/src/Pages/Admin.js
+++ front-end/src/Pages/Admin.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Header from '../Components/Header';
+import TableUsers from '../Components/TableUsers';
+import FormCreateUser from '../Components/FormCreateUser';
 
 function Admin() {
   return (
     <div>
       <h1>Administrador</h1>
+      <Header />
+      <FormCreateUser />
+      <TableUsers />
     </div>
   );
 }
