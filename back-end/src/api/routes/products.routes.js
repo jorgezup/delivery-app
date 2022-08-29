8e88b8bc3437a509f4dcb93d1b6f4c2a0c00dd52
Leticia Missias <leticia.m.icm@gmail.com>
2022-08-29 18:25:05 -0300

    devolve autenticação para o local correto

--- back-end/src/api/routes/products.routes.js
+++ back-end/src/api/routes/products.routes.js
@@ -1,9 +1,9 @@
 const express = require('express');
-// const authentication = require('../middlewares/authentication');
+const authentication = require('../middlewares/authentication');
 const productsController = require('../controllers/products.controller');
 
 const routes = express.Router();
 
-routes.get('/', productsController.getAllProducts);
+routes.get('/', authentication, productsController.getAllProducts);
 
 module.exports = routes;
