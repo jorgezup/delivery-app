a083e3a6b9037f2bca8a91ce48691bbbaa8c0eb6
Fábio Sabah <fabiosaba@gmail.com>
2022-08-30 00:35:22 -0300

    feat: middleware order joi

--- back-end/src/api/routes/orders.routes.js
+++ back-end/src/api/routes/orders.routes.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const authentication = require('../middlewares/authentication');
 const orderController = require('../controllers/orders.controller');
+const validateOrder = require('../middlewares/validateOrder');
 
 const routes = express.Router();
 
-routes.post('/', orderController.createOrder)
+routes.post('/', [
+  authentication,
+  validateOrder,
+  orderController.createOrder
+]);
+
 routes.get('/clients/:id', 
 authentication,
 orderController.getAllOrdersByClient);
