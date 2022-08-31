96d025c32388ca9675ec4c968f62dc2f7d3ebb32
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-08-31 15:25:35 -0400

    feat: added route orders details

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -14,14 +14,14 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Redirect exact from="/" to="/login" />
-          <Route path="/login" component={ Login } />
+          <Route exact path="/login" component={ Login } />
           <Route path="/customer/products" component={ Custumer } />
           <Route path="/seller/orders" component={ Seller } />
           <Route path="/admin/manage" component={ Admin } />
           <Route path="/register" component={ Register } />
-          <Route path="/customer/orders" component={ Orders } />
           <Route path="/customer/checkout" component={ Custumer } />
           <Route path="/customer/orders/:id" component={ OrderCustumerDetails } />
+          <Route exact path="/customer/orders" component={ Orders } />
         </Switch>
       </BrowserRouter>
     </div>
